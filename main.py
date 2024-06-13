cfd0a6d2a0112fc6721a1d2b02db525f4cce2d0c
Samuel Damasceno <156391711+samueldamasceno@users.noreply.github.com>
2024-06-13 19:20:08 -0300

    primeira versão do jogo funcionando

--- main.py
+++ main.py
@@ -29,11 +29,13 @@ def regras():
     print()
     print("Regras do jogo:")
     print()
-    print("""O jogo irá escolher aleatoriamente uma palavra, e o seu objetivo é descobrí-la.
+    print("""O jogo irá escolher aleatoriamente uma palavra, e o seu objetivo é descobri-la.
           Você pode chutar alguma letra que você acha que a palavra possui.
           Se você acertar a letra, ela aparecerá na palavra.
           Se você acertar todas as letras, você ganha!
-          Se você acertar alguma letra que a palavra não possui, você perde uma vida!
+          Se você chutar alguma letra que a palavra não possui, você perde uma vida!
+          Você também pode chutar uma palavra que você acha ser a palavra secreta.
+          Mas se você errar, você perde uma vida!
           Você terá 5 vidas.
           Vamos lá?""")
     print()
@@ -64,32 +66,60 @@ def jogo_forca():
     print()
 
     vidas = 5
+
+    letras_palavra = ["_"] * len(palavra_secreta)
+    letras_erradas = []
+
     while vidas > 0:
         print(f"Você possui {vidas} vidas")
+        
+        print("Palavra secreta: ", end="")
+        for letra in letras_palavra:
+            print(letra, end=" ")
         print()
 
-        for i in range(0, len(palavra_secreta)):
-            print("_", end=" ")
-        print()
-        print("Digite uma letra ou chute a palavra")
-        chute = input()
-        if len(chute) > 1:
+        if len(letras_erradas) > 0:
+            print("Letras erradas: ", end="")
+            for letra in letras_erradas:
+                print(letra, end=" ")
+            print()
+    
+        print("Digite uma letra ou chute a palavra:")
+        chute = input().lower()
+
+        if len(chute) == 1:
+            if chute in letras_palavra or chute in letras_erradas:
+                print("Você já chutou essa letra.")
+                continue
+
+            if chute in palavra_secreta:
+                for i in range(len(palavra_secreta)):
+                    if palavra_secreta[i] == chute:
+                        letras_palavra[i] = chute
+                if "_" not in letras_palavra:
+                    print()
+                    print(f"Parabéns! A palavra secreta era '{palavra_secreta}'. Você acertou!")
+                    print()
+                    break
+            else:
+                letras_erradas.append(chute)
+                vidas -= 1
+                print(f"A palavra secreta não tem a letra '{chute}'")
+
+        elif len(chute) > 1:
             if chute == palavra_secreta:
                 print()
-                print(f"Parabéns! A palavra secreta era {palavra_secreta}. Você acertou!")
+                print(f"Parabéns! A palavra secreta era '{palavra_secreta}'. Você acertou!")
                 print()
                 break
             else:
-                print()
-                print(f"Você errou! A palavra secreta não é {chute}")
-                print()
                 vidas -= 1
-                continue
-        elif len(chute) < 1:
-            print()
-            print("Digite uma letra ou chute a palavra")
-            continue
+                print()
+                print(f"Você errou! A palavra secreta não é '{chute}'")
+        
+        print()
 
-
+    if vidas == 0:
+        print(f"Você perdeu! A palavra secreta era '{palavra_secreta}'.")
 
 jogo_forca()
